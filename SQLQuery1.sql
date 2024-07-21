62cf797a018a6aee09c9ef917bfc076232211283
Maira Kamel <63196582+TheMeera-comp@users.noreply.github.com>
2024-07-21 12:30:40 +0300

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,14 +1,2 @@
-use db2024;
-
-CREATE TABLE SHEIN_Dresses (
-   DressCode int NOT NULL primary key,
-   DressSize nvarchar(10),
-   DressColor nvarchar(50),
-   DressLength nvarchar(50),
-   IsLongSleeve bit,
-   ISHalfSleeve bit, 
-   IsBackless bit,
-
-);
-
-select * from SHEIN_Dresses;
+INSERT INTO  (column1, column2, column3, ...)
+VALUES (value1, value2, value3, ...);
